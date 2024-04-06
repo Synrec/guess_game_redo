1e84cf3c0df48a1fb4dadb534789d85f89a1211e
Synrec <kylestclr@gmail.com>
2024-04-06 10:43:43 -0400

    Basic Guess Game

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use std::io;
+
+fn main (){
+    println!("Guess the number!");
+    println!("Your Input?");
+    let mut guess = String::new();
+    let _correct = 5;
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line.");
+    println!("You guessed: {guess}");
+    println!("Correct answer is: {_correct}");
+}
