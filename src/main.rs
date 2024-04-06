3c326dffbbc7819c4afffe356127e512faa1eea4
Synrec <kylestclr@gmail.com>
2024-04-06 11:20:01 -0400

    Clear space in number input using replace

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main (){
     println!("Guess the number!");
     println!("Your Input?");
     let mut guess = String::new();
-    let _correct = 5;
+    let _correct = rand::thread_rng().gen_range(1..=10);
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line.");
+    let guess: u32 = guess.trim().replace(" ", "").parse().expect("Please Type A Valid Number!!!!!");
     println!("You guessed: {guess}");
-    println!("Correct answer is: {_correct}");
+    match guess.cmp(&_correct){
+        Ordering::Less => println!("{guess} is too small. Correct number is: {_correct}"),
+        Ordering::Greater => println!("{guess} is too big. Correct number is: {_correct}"),
+        Ordering::Equal => println!("{guess} is equal to: {_correct}, you win!")
+    }
     println!("Thanks for playing!");
 }
